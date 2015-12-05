4bf1dd7aa76fa1a8b01d00e2811b7ed63db52c76
BadenMorgan <baden.david.morgan@gmail.com>
2015-12-05 14:53:43 +0200

    Added files
    Added libraries and test file that has been in progress for some time
    now, finally happy with uploading it

---
+++ ESP_MQTTClient-Strings/ESP8266.h
@@ -0,0 +1,89 @@
+/*
+Author: Baden Morgan
+
+ESP_MQTTClient
+A client program for the ESP8266 designed for the
+Arduino platform but can be ported to straight C
+for other microcontrollers.
+
+Code used to operate the eps8266 which is running
+the firmaware version 0.9.2.2 AT
+view datasheet for AT commands and descriptions
+https://room-15.github.io/blog/2015/03/26/esp8266-at-command-reference/
+
+For further instructions on how to setup
+Your ESP8266 follow the instructions at:
+http://www.xess.com/blog/esp8266-reflash/
+
+The library softreset was used to help with
+the connection sequence and automating its
+functionality, the library can be found here
+but has been included with this library
+adn can be found as a standalone here:
+https://github.com/WickedDevice/SoftReset
+
+The code relating to mqtt is all based
+on the documentation of mqtt v3.1.1
+
+The module was connecting to a mosquitto
+(v1.4.4) server over a local network,
+your results may differ depending on
+the server you use, please make sure its
+not the server before logging an issue.
+
+The functionality was also tested on an
+cloudmqtt server successfully.
+
+Please note that This code is not
+intended for sale by anyone and is
+opensource. If you do intend to use
+this software all I ask is that you
+give me credit were it is due. By
+downloading and using this you accept
+to not charge for this software.
+
+This software is provided by the copyright holders and contributors "as is" 
+and any express or implied warranties, including, but not limited to, the 
+implied warranties of merchantability and fitness for a particular purpose 
+are disclaimed. In no event shall the copyright owner or contributors be 
+liable for any direct, indirect, incidental, special, exemplary, or 
+consequential damages (including, but not limited to, procurement of 
+substitute goods or services; loss of use, data, or profits; or business 
+interruption) however caused and on any theory of liability, whether in 
+contract, strict liability, or tort (including negligence or otherwise) 
+arising in any way out of the use of this software, even if advised of the 
+possibility of such damage. 
+*/
+#include "Arduino.h"
+#ifndef ESP8266_h
+#define ESP8266_h
+#include "SoftReset.h"
+
+extern byte retries;
+
+class ESP8266 {
+
+  public:
+    ESP8266();
+    ESP8266(int SetTimeOut);
+    void InitComms();
+    void Connect(String SSID,String Pass);
+    void MQTTConnect(String broker, int port, String DeviceID);
+    void MQTTConnect(String broker, int port, String DeviceID, String Username, String Password);
+    void MQTTDisconnect();
+    void MQTTPublish(byte deviceNo, String message);
+    void MQTTPublish(String topic, String message);
+    void MQTTSubscribe(String topic);
+    String MQTTSubCheck();
+    void idler();
+    void DebugPrint(String msg);
+    byte WifiCheck(String SSID);
+    byte RTNConnected();
+  private:
+    boolean connectWiFi(String SSID,String Pass);
+    void ClearIncomingSerial();
+    void ReadSerial();
+    
+};
+
+#endif
---
+++ ESP_MQTTClient-Strings/examples/ESP8266_test/ESP8266_test.ino
@@ -0,0 +1,138 @@
+//#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <ESP8266.h>
+
+#define SSID "MorganOnly"
+#define Pass "1234m0rg@n2015"
+
+#define SERVER "10.0.0.23" //"m11.cloudmqtt.com"  //host for mosquitto broker
+#define PORT 1883  //18675  //port number for mosquitto broker
+//#define password "S3i9xIc5KAJF"
+//#define username "ubexgazz"
+#define ID "esp_device0"
+
+int count = 0;
+
+uint32_t stamp = 0;
+uint32_t stamp3 = 0;
+uint32_t stamp4 = 0;
+
+int PublishInterval = 5000;
+
+ESP8266 esp8266(500);
+
+byte retries = 0;
+byte allretries = 0;
+
+void setup() {
+  pinMode(7, OUTPUT);
+  pinMode(12, OUTPUT);
+  initESP8266();
+}
+
+void loop() {
+  if (esp8266.RTNConnected()) {
+    digitalWrite(7, HIGH);
+  } else {
+    digitalWrite(7, LOW);
+  }
+  MQTTProcess();
+}
+
+//////////////////////////
+//mqtt looping function
+/////////////////////////
+void MQTTProcess() {
+  byte connectdCheck = esp8266.RTNConnected();
+  if (connectdCheck == 1) {
+    if (millis() - stamp4 >= 1000) {//play around with shortening
+      stamp4 = millis();
+      SubExec(esp8266.MQTTSubCheck());
+    }
+    if (millis() - stamp3 >= PublishInterval) {
+      stamp3 = millis();
+      PublishQue();
+    }
+
+  }
+  if (!connectdCheck && (millis() - stamp >= 300000)) {
+    //esp8266.MQTTConnect(SERVER, PORT, ID, username, password);
+    esp8266.MQTTConnect(SERVER, PORT, ID);
+    retries = 0;
+    allretries++;
+    if (!esp8266.RTNConnected()) {
+      if (allretries == 3) {
+        initESP8266();
+        allretries = 0;
+      }
+      stamp = millis();
+    } else {
+      esp8266.MQTTSubscribe("hello");
+    }
+  }
+  if (connectdCheck != esp8266.RTNConnected()) {
+    stamp = millis();
+    if (esp8266.RTNConnected() == 0) {
+      esp8266.MQTTDisconnect();
+    }
+  }
+}
+
+/////////////////////////////////////////
+//esp initialization and broker connect
+/////////////////////////////////////////
+void initESP8266() {
+  if(!esp8266.WifiCheck(SSID)){
+    esp8266.Connect(SSID, Pass);
+  }
+  //esp8266.MQTTConnect(SERVER, PORT, ID, username, password);
+  esp8266.MQTTConnect(SERVER, PORT, ID);
+  if (esp8266.RTNConnected() == 1) {
+    SubhQue();    
+  }
+}
+
+////////////////////////////////////////
+//ESP subscription handling function
+///////////////////////////////////////
+/*first parameter is the
+ * message received from
+ * the library
+ */
+void SubExec(String receivedmsg) {
+  if (receivedmsg != "") {
+    byte sublen = receivedmsg[0];
+    byte topiclen = receivedmsg[2];
+    String topic;
+    for (int i = 3 ; i < topiclen + 3; i ++) {
+      topic += receivedmsg[i];
+    }
+    if (topic == "hello") {
+      digitalWrite(12, receivedmsg[topiclen + 3] - 48);
+    }
+  }
+}
+
+
+/////////////////////////
+//publish messages
+////////////////////////
+/*put your publish topics and
+ *messages in this function
+*/
+void PublishQue() {
+  String msg = "hello world";
+  String topic = "device/0";
+  msg += count;
+  count++;
+  esp8266.MQTTPublish(topic, msg);
+}
+
+/////////////////////////
+//Subscribe topics list
+////////////////////////
+void SubhQue(){
+  esp8266.MQTTSubscribe("hello");  
+}
+
